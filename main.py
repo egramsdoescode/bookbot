486e9a9ed67d48da73abf918a51d6e320b02d195
egramsdoescode <ethandoescode@gmail.com>
2025-12-30 17:03:13 -0600

    Add functionality to read book text and print it

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    file_contents = None
+
+    with open(filepath) as f:
+        file_contents = f.read()
+
+    print(file_contents)
+
+
+get_book_text("books/frankenstein.txt")
