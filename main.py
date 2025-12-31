0c7c48feaaea891407c77d97e19c1bb9cae2003c
egramsdoescode <ethandoescode@gmail.com>
2025-12-31 09:19:16 -0600

    Add function to generate report

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from stats import get_book_text, find_total_words, find_char_frequency
+from stats import (
+    get_book_text,
+    find_total_words,
+    find_char_frequency,
+    format_freq_map,
+    generate_report,
+)
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(f" Found {find_total_words(book_text)} total words")
-    print(find_char_frequency(book_text))
+    filepath = "books/frankenstein.txt"
+    book_text = get_book_text(filepath)
+    word_count = find_total_words(book_text)
+    char_frequency = find_char_frequency(book_text)
+
+    map_list = format_freq_map(char_frequency)
+
+    generate_report(filepath, word_count, map_list)
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -22,3 +22,32 @@ def find_char_frequency(boot_text):
             freq_map[lower_c] = 1
 
     return freq_map
+
+
+def format_freq_map(map):
+    def sort_on(items):
+        return items["num"]
+
+    result = []
+
+    for k, v in map.items():
+        if k.isalpha():
+            result.append({"char": k, "num": v})
+
+    result.sort(reverse=True, key=sort_on)
+
+    return result
+
+
+def generate_report(filepath, word_count, map_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book count at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for map in map_list:
+        char, num = map.items()
+        print(f"{char[1]}: {num[1]}")
+
+    print("============= END ===============")
