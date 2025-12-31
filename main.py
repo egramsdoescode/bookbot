28614ba98ede5008930455426d1c902eb103e66c
egramsdoescode <ethandoescode@gmail.com>
2025-12-31 08:24:19 -0600

    Add function to build freq map of characters

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from stats import get_book_text, find_total_words
+from stats import get_book_text, find_total_words, find_char_frequency
 
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     print(f" Found {find_total_words(book_text)} total words")
+    print(find_char_frequency(book_text))
 
 
-main()
+if __name__ == "__main__":
+    main()
--- stats.py
+++ stats.py
@@ -6,5 +6,19 @@ def get_book_text(filepath):
 
     return file_contents
 
+
 def find_total_words(book_text):
-    return len(book_text.split()) 
+    return len(book_text.split())
+
+
+def find_char_frequency(boot_text):
+    freq_map = {}
+
+    for c in boot_text:
+        lower_c = c.lower()
+        if lower_c in freq_map:
+            freq_map[lower_c] += 1
+        else:
+            freq_map[lower_c] = 1
+
+    return freq_map
